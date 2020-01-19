2abf02177d0f6358eda3ccc804791eb407f7a234
Brock <brock.reece@croud.co.uk>
2020-01-19 20:46:59 +0000

    Use process.env syntax

--- plugins/apollo.js
+++ plugins/apollo.js
@@ -1,7 +1,7 @@
 export default function (context) {
-  console.log(context.env)
+  console.log(process.env)
   return {
     httpEndpoint: 'https://graphql.datocms.com',
-    getAuth: () => `bearer ${context.env.DATO_CMS_TOKEN}`
+    getAuth: () => `bearer ${process.env.DATO_CMS_TOKEN}`
   }
 }
