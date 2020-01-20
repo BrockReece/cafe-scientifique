e91c31ff0ede5fc699b7754fe727fdf49b7799cc
Brock <brock.reece@croud.co.uk>
2020-01-20 17:31:57 +0000

    Remove debugging

--- plugins/apollo.js
+++ plugins/apollo.js
@@ -1,5 +1,4 @@
 export default function (context) {
-  console.log(process.env)
   return {
     httpEndpoint: 'https://graphql.datocms.com',
     getAuth: () => `bearer ${process.env.DATO_CMS_TOKEN}`
