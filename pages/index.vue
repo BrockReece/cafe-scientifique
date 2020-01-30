66818e0187ade18bcdfbb1704a5d52c742e9167a
Brock Reece <brockreece@gmail.com>
2020-01-30 21:00:47 +0000

    Latest change

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,11 @@
               src="~/assets/logo.png"
               height="194"
             >
-            <div class="headline pa-3">
+            <div class="headline pa-3 pb-1">
               Cafe Scientifique
             </div>
-            <div class="title mb-5">
+            <div>Shrewsbury</div>
+            <div class="title mt-3 mb-5">
               Interesting and insightful exploration of contemporary science issues
             </div>
             <div class="mb-3">
@@ -20,9 +21,9 @@
                   mdi-facebook
                 </v-icon>
               </v-btn>
-              <v-btn large icon class="ma-2">
+              <v-btn large icon class="ma-2" href="mailto:cafescishrewsbury@gmail.com">
                 <v-icon>
-                  mdi-twitter
+                  mdi-email
                 </v-icon>
               </v-btn>
             </div>
@@ -52,6 +53,10 @@
                 height="194"
               />
 
+              <v-card-title>{{ event.title }}</v-card-title>
+              <v-card-subtitle class="subtitle-1">
+                {{ event.speaker.name }}
+              </v-card-subtitle>
               <v-card-text v-html="$md.render(event.description)" />
 
               <v-card-actions>
@@ -82,10 +87,14 @@ export default {
           allEvents(orderBy: [ date_ASC ] first: 3, filter: $filter) {
             id
             date
+            title
             description
             bookingLink
             previewImage {
               url
+            }
+            speaker {
+              name
             }
           }
         }
